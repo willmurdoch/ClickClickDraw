1abbb024546e49efa52545bb480167a535b3c31e
Will Murdoch <wmurdoch@brownsteingroup.com>
2017-04-04 10:03:23 -0400

    Work is all in, working on lazy load

--- wp-content/themes/clickclickdraw/js/main.js
+++ wp-content/themes/clickclickdraw/js/main.js
@@ -1,6 +1,65 @@
-var $ = jQuery.noConflict();
+var $ = jQuery.noConflict(), myFilter = '', offset = 0, lazyLoop;
 $(document).ready(function(){
+
   $('.burger-btn').click(function(){
     $(this).parents('nav#mainNav').toggleClass('expanded');
   });
+
+  $('.work-filters a').click(function(e){
+    e.preventDefault();
+    if(!$(this).hasClass('active')){
+      $(this).addClass('active').siblings().removeClass('active');
+      myFilter = $(this).attr('data-category');
+      $('.work-grid').css('height', $('.work-grid').height()).html('<div class="loading"></div>');
+      filterContent('?ajax&category='+myFilter, '.work-grid');
+      offset = 0;
+    }
+  });
+
+  if($('.work-tile').length){
+    lazyLoopInit();
+  }
+
 });
+
+function lazyLoopInit(){
+  lazyLoop = setInterval(function(){
+    if($('.work-tile').length && $('.work-tile').last().offset().top - $(window).scrollTop() < $(window).height()){
+      offset += 8;
+      lazyLoad('.work-tile', '.work-grid');
+    }
+  }, 100);
+}
+
+function lazyLoad(child, content){
+  if(myFilter != ''){
+    var myUrl = window.location.href+'?ajax&category='+myFilter+'&offset='+offset;
+  }
+  else var myUrl = window.location.href+'?ajax&offset='+offset;
+  $.ajax({
+    url: myUrl,
+    type: 'GET',
+    dataType: 'html',
+    success: function(data){
+      var newContent = $(data).find(child);
+      if($(newContent).length > 0){
+        $(content).append(newContent);
+      }
+      else clearInterval(lazyLoop);
+    }
+  });
+}
+
+function filterContent(query, content){
+  var myUrl = window.location.href+query;
+  $.ajax({
+    url: myUrl,
+    type: 'GET',
+    dataType: 'html',
+    success: function(data){
+      var newContent = $(data).find(content);
+      $('.work-grid').html(newContent.children()).removeAttr('style');
+      lazyLoopInit();
+    }
+  });
+}
--- wp-content/themes/clickclickdraw/work.php
+++ wp-content/themes/clickclickdraw/work.php
@@ -7,21 +7,52 @@
 
  * @since Click Click Draw 2.0.0
  */
-get_header(); ?>
+if(!isset($_GET['ajax']))
+  get_header(); ?>
  <div id="wrapper" class="single-work">
  <?php
  if(have_posts()):
    while(have_posts()): the_post();
+    if(!isset($_GET['ajax'])):
     $featuredImage = wp_get_attachment_url(get_post_thumbnail_id()); ?>
      <div class="hero"><img src="<?php echo $featuredImage; ?>" alt="" /></div>
      <div class="scaler">
        <header>
          <h1 class="title work"><span>Work</span></h1>
        </header>
+       <div class="work-filters">
+         <a <?php if(!isset($_GET['category'])): ?>class="active"<?php endif; ?> href="#" data-category="">All</a>
+         <?php
+         $filterParent = get_cat_id('filters');
+         $args = array(
+           'parent' => $filterParent
+         );
+         $myCats = get_categories($args);
+         foreach($myCats as $cat){
+           if(isset($_GET['category']) && $_GET['category'] == $cat->slug){
+             echo '<a href="#" class="active" data-category="'.$cat->slug.'">'.$cat->name.'</a>';
+           }
+           else echo '<a href="#" data-category="'.$cat->slug.'">'.$cat->name.'</a>';
+         }
+         ?>
+       </div>
+      <?php endif; ?>
        <div class="work-grid">
-         <?php $args = array(
+         <?php
+         if(isset($_GET['offset'])) $offset = $_GET['offset'];
+         else $offset = 0;
+         if(isset($_GET['category'])){
+           $args = array(
+             'post_type' => 'work',
+             'posts_per_page' => 8,
+             'offset' => $offset,
+             'category_name' => $_GET['category']
+           );
+         }
+         else $args = array(
            'post_type' => 'work',
-           'posts_per_page' => -1
+           'posts_per_page' => 8,
+           'offset' => $offset
          );
          $myWork = get_posts($args);
          foreach($myWork as $work):
@@ -52,10 +83,14 @@ get_header(); ?>
            <?php echo '</a>';
          endforeach; ?>
        </div>
+    <?php if(!isset($_GET['ajax'])): ?>
      </div>
-  <?php endwhile;
+   <?php endif;
+   endwhile;
   endif;
 
-  include 'inc/contact.php'; ?>
+  if(!isset($_GET['ajax'])) include 'inc/contact.php'; ?>
  </div>
- <?php get_footer(); ?>
+ <?php
+ if(!isset($_GET['ajax']))
+  get_footer(); ?>
